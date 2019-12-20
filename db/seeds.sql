afa7efc5a4361f931385b04eb8fcb164e1fee4d6
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-12-19 20:28:08 -0500

    Major changes in the codebase.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+select * from jobs;
+
+insert into jobs (job_title, job_state, job_city, job_salary, job_description, job_requirements)
+values ("Entry_Level Software Engineer", "New Jersey", "Jersey City", 65000, 
+"This is an entry level developer position to assist in the development 
+and maintenance of web applications and/or enterprise systems.", "MySQL, Javascript, Reat.JS")
