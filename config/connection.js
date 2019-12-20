cf7bc4f9fe9db391597b90cfffa8ae7181644e1e
Felix Fernandez <fernandezfelix460.gmail.com>
2019-12-19 19:04:38 -0500

    test fixses

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,8 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "",
-        database: "",
+        password: "password",
+        database: "job_seeker",
     });
 }
 
